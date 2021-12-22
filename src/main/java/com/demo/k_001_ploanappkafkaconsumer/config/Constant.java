591c4187d10a986563f0a70d5ff7725073da7efe
Aekcom <banhaan.swangwaan@priorsolution.co.th>
2021-12-22 16:02:55 +0700

    update code

--- src/main/java/com/demo/k_001_ploanappkafkaconsumer/config/Constant.java
+++
@@ -1,7 +0,0 @@
-package com.demo.k_001_ploanappkafkaconsumer.config;
-
-public class Constant {
-
-    public static final String PLOAN_APP_TOPIC = "kkp-ploan-app-naos-sample";
-
-}
--- src/main/java/com/demo/k_001_ploanappkafkaconsumer/model/SampleRequest.java
+++ src/main/java/com/demo/k_001_ploanappkafkaconsumer/model/SampleRequest.java
@@ -5,8 +5,6 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import java.io.Serializable;
-
 @NoArgsConstructor(access = AccessLevel.PUBLIC)
 @Getter(AccessLevel.PUBLIC)
 @Setter(AccessLevel.PUBLIC)
